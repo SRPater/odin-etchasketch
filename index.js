50f4a2874b4db566a658b2a24d6e82290eba9146
SRPater <srpater@gmail.com>
2026-01-12 13:43:47 +0100

    Extract new grid functionality

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.width = container.offsetWidth / 16 + "px";
-    square.style.height = container.offsetHeight / 16 + "px";
-    container.appendChild(square);
+function newGrid(sideLength) {
+    container.innerHTML = "";
+
+    for (let i = 0; i < sideLength * sideLength; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = container.offsetWidth / sideLength + "px";
+        square.style.height = container.offsetHeight / sideLength + "px";
+        container.appendChild(square);
+    }
+
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "grey";
+        })
+    });
 }
 
-let squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "grey";
-    });
-});
-
-const newGrid = document.querySelector("#new");
-newGrid.addEventListener("click", () => {
+const button = document.querySelector("#new");
+button.addEventListener("click", () => {
     const input = parseInt(prompt("How many squares per side?", "16"));
 
     if (isNaN(input) || input > 100 || input < 2) {
         alert("Please enter a number from 2 to 100!");
     } else {
-        while (container.firstChild) {
-            container.firstChild.remove();
-        }
-
-        for (let i = 0; i < input * input; i++) {
-            const square = document.createElement("div");
-            square.classList.add("square");
-            square.style.width = container.offsetWidth / input + "px";
-            square.style.height = container.offsetHeight / input + "px";
-            container.appendChild(square);
-        }
-
-        squares = document.querySelectorAll(".square");
-            squares.forEach((square) => {
-            square.addEventListener("mouseover", () => {
-                square.style.backgroundColor = "grey";
-            });
-        });
+        newGrid(input);
     }
-})
+});
+
+newGrid(16);
